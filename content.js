aa925565ce971d909a1f24244f9cf641ec81e5b2
Andy Shora <andyshora@gmail.com>
2012-11-05 09:54:54 +0000

    already interested fix, bookmarking tested

--- content.js
+++ content.js
@@ -1,3 +1,15 @@
 console.log("Loading Beast…");
 
-$('.floater,#ad_overlay').hide();
+$('div.vipboxads')
+	.html("<h1 style=\"color:white\">Beast Mode Activated</h1>");
+
+$('div.vipboxads').next('.black').children().each(function(){
+
+	if ($(this).css('position')!=='relative'){
+		console.log("MUNCH");
+		$(this).remove();
+	}
+});
+$('div.black>table').remove();
+$('.floater,#ad_overlay,#pageshare,#vipchat,#nochat').hide();
+$('#lightsOff').parents('.black').remove();
